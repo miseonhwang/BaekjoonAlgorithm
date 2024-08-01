f99f45749a537b92c401b68536d4f7f19939a88b
miseonhwang <miseon.til@gmail.com>
2024-08-01 18:54:21 +0900

    [Silver IV] Title: 상근이의 여행, Time: 260 ms, Memory: 31132 KB -BaekjoonHub

---
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int t = 0;
+        int[][] board;
+
+        t = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < t; i++) {
+            st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            int m = Integer.parseInt(st.nextToken());
+            for (int j = 0; j < m; j++) {
+                st = new StringTokenizer(br.readLine());
+            }
+            System.out.println(n - 1);
+        }
+    }
+}
